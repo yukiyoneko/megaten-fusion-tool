90aa0c0e41eaffbd2384c3225a5bb0e7323e5603
yukiyoneko <empresslaila0@gmail.com>
2024-09-06 14:53:29 -0400

    Revert "test"
    This reverts commit 82606c06d89bab955fad22da6726215764368273.

--- docker/update-assets-subpath.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-find ../gh-pages/*.js -type f | xargs sed -i 's/\/assets\/images\//\/megaten-fusion-tool\/assets\/images\//g'
-find ../gh-pages/*.js -type f | xargs sed -i 's/\/assets\/js\//\/megaten-fusion-tool\/assets\/js\//g'
--- src/app/shared/position-edges.ts
+++
@@ -1,7 +0,0 @@
-export interface PositionEdges {
-  top: number;
-  bottom: number;
-  left: number;
-  right: number;
-  zIndex: number;
-}
